2df3f8de18e13548a230e0aa33e0814fbfb0a4d3
SnowDev <snowcloudsdev@gmail.com>
2025-02-16 19:25:15 -0500

    Added Auto Push

---
+++ UpdateGithub.bat
@@ -0,0 +1,16 @@
+@echo off
+setlocal
+
+:: Ask for a commit message
+set /p commitMessage=Enter commit message: 
+
+:: Add all changes
+git add .
+
+:: Commit with user-provided message
+git commit -m "%commitMessage%"
+
+:: Push to the main branch
+git push origin main
+
+endlocal
